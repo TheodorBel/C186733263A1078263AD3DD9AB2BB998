9910c46c402c96ff3adc5ce2362a7f0006910ab2
TheodorBel <103110252+TheodorBel@users.noreply.github.com>
2022-05-08 16:36:46 +0500

    New changes

---
+++ lists/database/main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct s_node
+{
+    int num;
+    struct s_node *prev;
+    struct s_node *next;
+}t_node;
+
+typedef struct database{
+    int size;
+    t_node *first_node;
+}db_type;
+
+t_node *create_node(int value)
+{
+    t_node *new;
+    new = malloc(sizeof (t_node));
+    new->num = value;
+    new->next = NULL;
+    new->prev = NULL;
+    return (new);
+}
+
+db_type *create_database(){
+    db_type *new;
+    new = malloc(sizeof(db_type));
+    new->size = 0;
+    new->first_node = NULL;
+    return(new);
+};
+
+void db_insert(db_type *db, int aleksey){
+    t_node *newnode;
+    newnode = create_node(aleksey);
+    if (db->first_node == NULL) { //Получается так, что это получается нода, если не было ни одной, то она запишется как первая.
+        db->first_node = newnode;
+        db->size = db->size + 1;
+        return;
+    }
+    t_node *last;
+    last = db->first_node;
+    while (last->next != NULL){
+        last = last->next; // Слева то куда, справа то что присваиваю.
+    }
+    last->next = newnode;
+    newnode->prev = last;
+
+}
+
+int main() {
+    db_type *db = create_database();
+    db_insert(db, rand() % 8);
+    db_insert(db, rand() % 8 );
+//    t_node *head;
+//    head = malloc(sizeof (t_node));
+//    head->num = rand() % 5;
+//    head->next = NULL;
+//    head->index = 0;
+//    head ->prev = NULL;
+//    t_node *tmp;
+//    head->next = tmp;
+//    t_node *last;
+//    last = head;
+
+//    for (int i = 0; i < 10; i++)
+//    {
+//        tmp = create_node();
+//        tmp->index = last->index +1;
+//        tmp->prev = last;
+//        last->next = tmp;
+//        last = tmp;
+//    }
+//
+    return 0;
+}
---
+++ oldhomework/LogPassV4.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <string.h>
+#include <stdio.h>
+#include <windows.h>
+
+int main(void)
+{
+	char datalogin[6] = "test1"; // Массивы для заданных пароля и логина
+	char datapassword[6] = "aboba";
+	char userlogin[16] = "00000"; // Массивы для ввода пользователя
+	char userpassword[16] = "aziza";
+	int usertry = 0;
+	while (usertry < 5) // Цикл для проверки введенных данных
+	{
+		++usertry;
+		printf ("your login:\n");
+		scanf ("%s", userlogin); //Ввод данных
+		printf ("your password:\n");
+		scanf ("%s", userpassword); //Еще один ввод
+		if ((strcmp(datalogin, userlogin) == 0) && (strcmp(datapassword, userpassword)) == 0) //Сравнение данных и ответ в случае правильного ввода
+		{
+			printf("hello admin!");
+			break;
+		}
+		else if ((strcmp(datalogin, userlogin) != 0) || (strcmp(datapassword, userpassword)) != 0) // Сравнение данных и ответ в случае неправильного ввода
+		{
+			printf("Incorrect\n");
+			if (usertry == 3)
+			{
+				printf("Wait 3 sec before try again\n");
+				Sleep(3000);
+			}
+		}
+	}
+	return 0;
+}
