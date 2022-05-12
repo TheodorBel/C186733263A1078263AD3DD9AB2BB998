6cf8e01789cc2409a98452dc5dae0698aaf2e3d8
TheodorBel <103110252+TheodorBel@users.noreply.github.com>
2022-05-13 01:05:57 +0500

    Now its final version
    And its working i swear

--- lists/database/main.c
+++ lists/database/main.c
@@ -32,6 +32,51 @@ db_type *create_database(){
     return(new);
 };
 
+t_node *db_find_by_index(db_type *db, int index){
+    t_node *finder = db->first_node;
+    int current = 0;
+    if ((db->size <= index) || (index < 0))
+        return(NULL);
+    while (current != index){
+        finder = finder->next;
+        current = current + 1;
+    }
+    return (finder);
+}
+
+int db_read_by_index(db_type *db,int index){
+    t_node *finder;
+    finder = db_find_by_index(db,index);
+    if(finder == NULL)
+        return(-1);
+    return(finder->num);
+}
+
+void db_delete_by_index(db_type *db, int index){
+    t_node *finder;
+    t_node *prev;
+    t_node *next;
+    finder = db_find_by_index(db,index);
+    if (finder == NULL)
+        return;
+    db->size = db->size -1;
+    prev = finder->prev;
+    next = finder->next;
+    if(index == 0){
+        db->first_node = finder->next;
+        free(finder);
+        return;
+    }
+    if(index == db->size) {
+        prev->next = NULL;
+        free(finder);
+        return;
+    }
+    prev->next = next;
+    next->prev = prev;
+    free(finder);
+}
+
 void db_insert_end(db_type *db, t_node *new){
     if (db->first_node == NULL) { //Получается так, что это получается нода, если не было ни одной, то она запишется как первая.
         db->first_node = new;
@@ -71,7 +116,7 @@ void db_insert_mid(db_type *db, t_node *new, int index)
     t_node *tmp;
     int i = 0;
     last = db->first_node;
-    while (last != NULL)
+    while (last != NULL) // Поиск по индексу
     {
         tmp = last->next;
         last = tmp;
@@ -108,11 +153,38 @@ void db_insert(db_type *db, int value, int index){
     db_insert_mid(db, new, index);
 }
 
+int db_getsize(db_type *db){
+    return(db->size);
+}
+
+t_node *db_get_element_by_value(db_type *db,int value){
+    t_node *finder = db->first_node;
+    if (db->size == 0)
+        return(NULL);
+    while (finder != NULL){
+        if(finder->num == value)
+            return(finder);
+        finder = finder->next;
+    }
+    return (NULL);
+
+}
+
 int main() {
+    int test_to_finder;
     db_type *db = create_database();
     db_insert(db,0,-1);
-    db_insert(db,2,-1);
+    db_insert(db,11,-1);
     db_insert(db,1,-1);
     db_insert(db,4,1);
+    db_insert(db,1,-1);
+    test_to_finder = db_read_by_index(db,3);
+    printf("Readden element:%d\nSize of my base:%d\n", test_to_finder, db_getsize(db));
+    db_delete_by_index(db,3);
+    test_to_finder = db_read_by_index(db,3);
+    printf("Readden element:%d\nSize of my base:%d\n", test_to_finder, db_getsize(db));
+
+
+
     return 0;
 }
